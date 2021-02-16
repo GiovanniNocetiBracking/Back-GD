bee09aa36cde65431ca2625b11c11e6b192766fc
Giovanni Noceti <giovanni.noceti@gmail.com>
2021-02-15 22:21:51 -0300

    validaciones por el lado del servidor listas

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,85 +1,91 @@
 'use strict'
 
 const Mail = use('Mail')
-const User = use('App/Models/User') 
+const User = use('App/Models/User')
+const { validate } = use('Validator') 
 
-class UserController {
- 
-  async index ({ request, response, view }) {
-
-  }
-  
-  async create ({ request, response, view }) {
-    
-  }
+class UserController {  
   async contactUs ({request}){
     try {
       const {name, email, subject, message } = request.all()
       const data = request.body
-
+      const rules = {
+        email: 'required|email',
+        name: 'required',
+        subject: 'required',
+        message: 'required'
+      }
+      const validation = await validate(request.all(), rules)
+      if (validation.fails()) {  
+          return console.log(validation)
+      }else{
       await Mail.send('email', data, (message) => {
         message
           .to('giovanni.noceti@gmail.com')
           .from('giovanni.noceti@gmail.com')
           .subject(subject)
-      })
-   
-    return console.log(data)
+      })   
+    return console.log(data)}
     } catch (error) {
-      console.log('hubo un error ' + error)
+      console.log(error)
     }
   }
-
   async login ({ request, auth, response}){
     try{
       const {email, password} = request.all()
+      const rules = {
+        email: 'required|email',        
+        password: 'required'
+      }
+      const validation = await validate(request.all(), rules)
+      if (validation.fails()) {  
+          return console.log(validation)
+      }      
       const token = await auth.attempt(email, password)
-      return token
+      return console.log(token)      
     }catch(e){
       return response.json(e.message)
+    }    
+  }  
+  async store ({ request, response }) {    
+    const {email, password, username, suscribe} = request.all()   
+    const rules = {
+      email: 'required|email|unique:users,email',        
+      password: 'required',
+      username: 'required'
     }
-    
-  }
-  
-  async store ({ request, response }) {
-    console.log('hello world')
-    const {email, password, username, suscribe} = request.all()
+    const validation = await validate(request.all(), rules)
+    if (validation.fails()) { 
+        return console.log(validation)
+    }
     await User.create({
       email,
       password,
       username,
       suscribe
     })
-      return this.login(...arguments)
+      return console.log(request.body)
   }
-
   async suscribe({request, response}){
     try{
-    const {email, suscribe} = request.all()
-    
+    const {email, suscribe} = request.all()      
+    const rules = {
+      email: 'required|email|unique:users, email',       
+    }
+    const validation = await validate(request.all(), rules)
+    if (validation.fails()) { 
+        return console.log(validation)
+    }     
     const newSuscriber = await User.create({
       email,
       suscribe
-    })    
-      return response.json(newSuscriber)        
+    }) 
+      return console.log(newSuscriber.$attributes)       
     }catch(e){
       return response.json(e.message)
     }
   }
-   
   
-  async show ({ params, request, response, view }) {
-    
-  }
- 
-  async edit ({ params, request, response, view }) {
-  }
-  
-  async update ({ params, request, response }) {
-  }
-  
-  async destroy ({ params, request, response }) {
-  }
 }
 
 module.exports = UserController
